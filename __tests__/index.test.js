81446a2483214e9a48bbcf45a67e1f4652c3aa49
Kristina Kovach <kristina.kovach@team.wrike.com>
2022-07-01 15:38:14 +0300

    [step 3] Fix linter issues

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,12 @@
-import path from 'path';
+import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import genDiff from "../src";
-import fs from "fs";
+import path from 'path';
+import fs from 'fs';
+import genDiff from '../src';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const getFixturePath = (filename) => path.join(dirname, '..', '__fixtures__', filename);
 const expectedResult = fs.readFileSync(getFixturePath('expected_result.txt'), { encoding: 'utf8' });
-
 
 test('genDiff', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expectedResult);
