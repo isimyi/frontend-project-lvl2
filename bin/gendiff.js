31dda6f52b38601a43885a4f85b5cbc2aa21bb8b
Kristina Kovach <kristina.kovach@team.wrike.com>
2022-08-03 22:41:12 +0300

    [step 6] Add default option to formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,11 +3,12 @@ import { Command } from 'commander/esm.mjs';
 import genDiff from '../src/index.js';
 
 const program = new Command();
+const options = program.opts();
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2) => { console.log(genDiff(filepath1, filepath2)); })
+  .option('-f, --formatter [type]', 'Choose output format', 'stylish')
+  .action((filepath1, filepath2) => { console.log(genDiff(filepath1, filepath2, options.formatter)); })
   .parse();
