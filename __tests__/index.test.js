5e81c9b38f9915baa695cdfbfd427475cdf995ee
Kristina Kovach <kristina.kovach@team.wrike.com>
2022-08-31 20:55:45 +0300

    [step 8] Add tests for json formatter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,10 +8,13 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const expectedResultStylish = fs.readFileSync(getFixturePath('expected_result_stylish.txt'), { encoding: 'utf8' });
 const expectedResultPlain = fs.readFileSync(getFixturePath('expected_result_plain.txt'), { encoding: 'utf8' });
+const expectedResultPlain = fs.readFileSync(getFixturePath('expected_result_json.json'), { encoding: 'utf8' });
 
 test('genDiff', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toEqual(expectedResultStylish);
   expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'stylish')).toEqual(expectedResultStylish);
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(expectedResultPlain);
   expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain')).toEqual(expectedResultPlain);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(expectedResultPlain);
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json')).toEqual(expectedResultPlain);
 });
