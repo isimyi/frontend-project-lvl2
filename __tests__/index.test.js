8603e0c1f937ec0cd0ee85fd655cb7d8bd0ca938
Kristina Kovach <kristina.kovach@team.wrike.com>
2022-09-05 23:40:28 +0300

    [step 8] tests: add each, check for json result parsing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,17 +4,21 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '../src';
 
+const extensions = ['json', 'yml'];
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const expectedResultStylish = fs.readFileSync(getFixturePath('expected_result_stylish.txt'), { encoding: 'utf8' });
 const expectedResultPlain = fs.readFileSync(getFixturePath('expected_result_plain.txt'), { encoding: 'utf8' });
 const expectedResultJson = fs.readFileSync(getFixturePath('expected_result_json.json'), { encoding: 'utf8' });
 
-test('genDiff', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expectedResultStylish);
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(expectedResultStylish);
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(expectedResultPlain);
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain')).toEqual(expectedResultPlain);
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(expectedResultJson);
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json')).toEqual(expectedResultJson);
+test.each(extensions)('genDiff', (extension) => {
+  const beforeFullPath = getFixturePath(`fileBefore.${extension}`);
+  const afterFullPath = getFixturePath(`fileAfter.${extension}`);
+  const jsonResult =  genDiff(beforeFullPath, afterFullPath, 'json');
+  
+  expect(genDiff(beforeFullPath, afterFullPath)).toEqual(expectedResultStylish);
+  expect(genDiff(beforeFullPath, afterFullPath, 'stylish')).toEqual(expectedResultStylish);
+  expect(genDiff(beforeFullPath, afterFullPath, 'plain')).toEqual(expectedResultPlain);
+  expect(jsonResult).toEqual(expectedResultJson);
+  expect(() => JSON.parse(jsonResult)).not.toThrow();
 });
